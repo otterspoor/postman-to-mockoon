9f3a979c32fcb4f6710e8be768724519cc76ef5c
Nathan Otterspoor <nathan.otterspoor@deakinco.com>
2020-04-04 20:35:43 +1100

    better handling of postman collection with mixed data

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Postman to Mockoon
 
+This repository provides a CLI tool for converting Postman collections (only v2 at the moment) to files that can be imported into Mockoon.
+
+## Usage
+
 `npm i -g otterspoor/postman-to-mockoon`
 
 `postman-to-mockoon input-file output-file`
--- index.js
+++ index.js
@@ -39,29 +39,36 @@ env.item.uuid = postman.info._postman_id;
 
 postman.item.forEach((collection) => {
   collection.item.forEach((r_data) => {
-    let route = new Route();
-    route.uuid = r_data.name;
-    route.method = r_data.request.method.toLowerCase();
-    let path = r_data.request.url.path;
-    route.endpoint = typeof path == "string" ? path : path.join("/");
-    if (r_data.response && r_data.response.length > 0) {
-      r_data.response.forEach((r) => {
-        let resp = new Response();
-        if (r.code) {
-          resp.statusCode = r.code.toString();
-        }
-        if (r.header) {
-          resp.headers = r.header;
-        }
-        if (r.body) {
-          resp.body = r.body;
-        }
-        route.addResponse(resp);
-      });
+    if (r_data.request && r_data.responses && r_data.responses.length == 0) {
+      // console.warn("Found request-only item, skipping...");
     } else {
-      route.addResponse(new Response());
+      if(r_data.response){
+        r_data.responses = r_data.response;
+      }
+      let route = new Route();
+      route.documentation = r_data.name;
+      route.method = r_data.request.method.toLowerCase();
+      let url = r_data.request.url;
+      route.endpoint = typeof url == "string" ? url : url.path.join("/");
+      if (r_data.responses && r_data.responses.length > 0) {
+        r_data.responses.forEach((r) => {
+          let resp = new Response();
+          if (r.code) {
+            resp.statusCode = r.code.toString();
+          }
+          if (r.header) {
+            resp.headers = r.header;
+          }
+          if (r.body) {
+            resp.body = r.body;
+          }
+          route.addResponse(resp);
+        });
+      } else {
+        route.addResponse(new Response());
+      }
+      env.item.addRoute(route);
     }
-    env.item.addRoute(route);
   });
 });
 
